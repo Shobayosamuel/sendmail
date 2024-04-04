20b733233fb671a50ae1b6fa067428d1bf981ffa
shobayo samuel <shobayosamuel62@gmail.com>
2024-04-04 21:10:40 +0100

    send mail

--- email_sender/views.py
+++ email_sender/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from .serializers import EmailSerializer
 from .utils import send_email
 
+
 class SendEmailView(APIView):
     def post(self, request):
         serializer = EmailSerializer(data=request.data)
